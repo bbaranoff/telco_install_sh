5929f0d2071e50b41b2d78a992efa059121edfdc
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-04-24 01:26:36 +0200

    Update install_kernel.sh

--- install_kernel.sh
+++ install_kernel.sh
@@ -8,6 +8,7 @@ wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-headers-5.3.0-
 wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-headers-5.3.0-050300-generic_5.3.0-050300.201909152230_$ARCH.deb
 wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-image-unsigned-5.3.0-050300-generic_5.3.0-050300.201909152230_$ARCH.deb
 wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-modules-5.3.0-050300-generic_5.3.0-050300.201909152230_$ARCH.deb
+apt remove linux-*
 sudo dpkg -i *.deb
 read -p "reboot and choose kernel 5.3 ? Ctrl-C to exit/Enter to reboot"
 reboot
