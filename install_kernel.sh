39af73ab4310ea3fcf2faf91ef2682f10f8f4d58
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-04-23 16:03:06 +0200

    Create install_kernel.sh

---
+++ install_kernel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt update && apt upgrade
+rm -rf /opt/GSM
+mkdir /opt/GSM
+cd /opt/GSM
+read -p "Architecture arm64 or amd64 ?"
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-headers-5.3.0-050300_5.3.0-050300.201909152230_all.deb
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-headers-5.3.0-050300-generic_5.3.0-050300.201909152230_$ARCH.deb
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-image-unsigned-5.3.0-050300-generic_5.3.0-050300.201909152230_$ARCH.deb
+wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-modules-5.3.0-050300-generic_5.3.0-050300.201909152230_$ARCH.deb
+sudo dpkg -i *.deb
+read -p "reboot and choose kernel 5.3 ? Ctrl-C to exit"
