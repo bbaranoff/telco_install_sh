a4d52907e817baa09b1f0c8633ba6c77311799c8
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-04-23 16:15:29 +0200

    Update install_kernel.sh

--- install_kernel.sh
+++ install_kernel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt update && apt upgrade -y
-rm -rf /opt/GSM
-mkdir /opt/GSM
-cd /opt/GSM
-read -p "Architecture arm64 or amd64 ?"
+rm -rf /opt/IMSI_Catcher
+mkdir /opt/IMSI_Catcher
+cd /opt/IMSI_Catcher
+read -p "Architecture arm64 or amd64 ?" ARCH
 wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-headers-5.3.0-050300_5.3.0-050300.201909152230_all.deb
 wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-headers-5.3.0-050300-generic_5.3.0-050300.201909152230_$ARCH.deb
 wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-image-unsigned-5.3.0-050300-generic_5.3.0-050300.201909152230_$ARCH.deb
 wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-modules-5.3.0-050300-generic_5.3.0-050300.201909152230_$ARCH.deb
 sudo dpkg -i *.deb
-read -p "reboot and choose kernel 5.3 ? Ctrl-C to exit"
+read -p "reboot and choose kernel 5.3 ? Ctrl-C to exit/Enter to reboot"
