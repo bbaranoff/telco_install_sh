29b8f1d3f46c4fb65e2b6022b0f6494b29e08e13
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-04-24 18:53:05 +0200

    Update install_kernel.sh

--- install_kernel.sh
+++ install_kernel.sh
@@ -3,7 +3,7 @@ apt update && apt upgrade -y
 rm -rf /opt/IMSI_Catcher
 mkdir /opt/IMSI_Catcher
 cd /opt/IMSI_Catcher
-apt remove linux-*
+apt remove linux-image*
 read -p "Architecture arm64 or amd64 ? " ARCH
 wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-headers-5.3.0-050300_5.3.0-050300.201909152230_all.deb
 wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.3/linux-headers-5.3.0-050300-generic_5.3.0-050300.201909152230_$ARCH.deb
