e163b9ad1899840c2f26275b59a783c2acb0e61d
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-04-24 03:33:18 +0200

    Update run_last.sh

--- run_last.sh
+++ run_last.sh
@@ -1,14 +1,2 @@
-d /root/nitb
-./nitb.sh &
-cd /opt/IMSI_Catcher/trx/src/host/layer23/src/transceiver
-sudo ./transceiver -a 4 -2 -r 99 &
 cd /opt/IMSI_Catcher/osmo-bts/doc/examples/calypso
 sudo osmo-bts-trx &
-lcr start &
-asterisk -cvvvvvvv &&
-sudo killall -9 asterisk
-sudo killall -9 lcr
-sudo killall -9 osmo-bts-trx
-sudo killall -9 transceiver
-sudo killall -9 osmo-nitb
-sudo killall -9 osmocon
