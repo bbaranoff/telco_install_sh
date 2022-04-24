87eda9901f2277e04da3f03cbebefeb9d4867997
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-04-24 18:00:49 +0200

    Update run_first.sh

--- run_first.sh
+++ run_first.sh
@@ -1,2 +1,2 @@
 cd /opt/IMSI_Catcher/trx/src
-./host/osmocon/osmocon -p /dev/ttyUSB0 -s /tmp/osmocom_l2 -m c123xor -c trx.highram.bin -r 99
+./host/osmocon/osmocon -p /dev/ttyUSB0 -s /tmp/osmocom_l2 -m c123 -c trx.highram.bin -r 99
