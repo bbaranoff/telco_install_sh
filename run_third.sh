ab772ccc8da84f5dc34ccff7857d93e5e91b1782
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-04-24 03:34:39 +0200

    Create run_third.sh

---
+++ run_third.sh
@@ -0,0 +1,15 @@
+cd /root/nitb
+modprobe mISDN_core
+modprobe mISDN_dsp
+modprobe snd-mixer-oss
+modprobe snd-pcm
+sudo ./nitb.sh &
+cd /opt/IMSI_Catcher/trx/src/host/layer23/src/transceiver
+sudo ./transceiver -a 4 -2 -r 99 &
+sudo lcr start &
+sudo asterisk -cvvvvvvvvv &&
+sudo killall -9 asterisk
+sudo killall -9 lcr
+sudo killall -9 transceiver
+sudo killall -9 osmo-nitb
+sudo killall -9 osmocon
