08fc111e07177df776bd13669a72078dfbe3e3d9
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-04-24 18:01:13 +0200

    Update run_sec.sh

--- run_sec.sh
+++ run_sec.sh
@@ -1,2 +1,2 @@
 cd /opt/IMSI_Catcher/trx/src
-./host/osmocon/osmocon -p /dev/ttyUSB1 -s /tmp/osmocom_l2.2 -m c123xor -c trx.highram.bin -r 99
+./host/osmocon/osmocon -p /dev/ttyUSB1 -s /tmp/osmocom_l2.2 -m c123 -c trx.highram.bin -r 99
